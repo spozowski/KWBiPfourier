7daa6b8a57b0468ae797a5bc584cebb1652749ef
spozowski <jakub.spozowski2@gmail.com>
2026-04-27 16:15:07 +0200

    Update Zadanie2.m

--- Zadanie2.m
+++ Zadanie2.m
@@ -16,6 +16,7 @@ t20 = t(1:N20);
 x20 = x(1:N20);
 
 figure(1)
+grid on;
 plot(t20,x20,'r')
 title('Wyskalowany przebieg czasowy')
 xlabel('Czas')
@@ -24,6 +25,7 @@ ylabel('Przemieszczenie, um')
 %% Zadanie 2 
 
 n = length(x);
+n_vector = 0:1:n;
 f = Fs*(0:(n/2)-1)/n;
 y = abs(fft(x))/(n/2);
 y = y(1:n/2);
@@ -37,17 +39,17 @@ grid on;
 
 %% Zadanie 3
 
-x1 = x(Fs*4:Fs*5);
-t1 = t(Fs*4:Fs*5);
-z1 = z(Fs*4:Fs*5);
+x1 = x(Fs*4:Fs*5-1);
+t1 = t(Fs*4:Fs*5-1);
+z1 = z(Fs*4:Fs*5-1);
 
-x2 = x(Fs*19:Fs*20);
-t2 = t(Fs*19:Fs*20);
-z2 = z(Fs*19:Fs*20);
+x2 = x(Fs*19:Fs*20-1);
+t2 = t(Fs*19:Fs*20-1);
+z2 = z(Fs*19:Fs*20-1);
 
-x3 = x(Fs*35:Fs*36);
-t3 = t(Fs*35:Fs*36);
-z3 = z(Fs*35:Fs*36);
+x3 = x(Fs*35:Fs*36-1);
+t3 = t(Fs*35:Fs*36-1);
+z3 = z(Fs*35:Fs*36-1);
 
 figure(3)
 subplot(3,1,1)
@@ -81,8 +83,59 @@ y2 = y2(1:n1/2);
 y3 = abs(fft(x3))/(n1/2);
 y3 = y3(1:n1/2);
 
-figure(4);hold on;
+figure(4);hold on;grid on;
 plot(f1,y1,'r')
 plot(f1,y2,'b')
 plot(f1,y3,'g')
+
+%% Zadanie 5
+
+%ilości próbek w przedziałach czasowych
+
+n1 = n_vector(Fs*4:Fs*5-1);
+n2 = n_vector(Fs*19:Fs*20-1);
+n3 = n_vector(Fs*35:Fs*36-1);
+
+figure(5)
+subplot(3,1,1)
+plot(n1,z1, 'r')
+title('Przebieg sygnału znacznika obrotów od 4 do 5 sekundy')
+xlabel('Ilość próbek')
+ylabel('Obroty')
+subplot(3,1,2)
+plot(n2,z2, 'r')
+title('Przebieg sygnału znacznika obrotów od 19 do 20 sekundy')
+xlabel('Ilość próbek')
+ylabel('Obroty')
+subplot(3,1,3)
+plot(n3,z3, 'r')
+title('Przebieg sygnału znacznika obrotów od 35 do 36 sekundy')
+xlabel('Ilość próbek')
+ylabel('Obroty')
+
+% f1,f2,f3 częstotliwości obrotowe odpowiadające sygnalom x1,x2,x3
+
+p1 = 10994 - 10668;
+a1 = p1/2;
+f1 = Fs/a1;
+
+p2 = 49172-49059;
+a2 = p2/2;
+f2 = Fs/a2;
+
+p3 = 90050 - 89976;
+a3 = p3/2;
+f3 = Fs/a3;
+
+%% Zadanie 6
+
+[S,F,T] = stft(x, Fs);
+
+figure;
+imagesc(T, F, abs(S));
+axis xy;
+xlabel('Czas [s]');
+ylabel('Częstotliwość [Hz]');
+title('Spektrogram (STFT)');
+colorbar;
 
